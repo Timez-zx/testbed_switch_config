c92647c69d8a1c21d82a3e392436e051058e05a3
zx <zhangxiao123@sjtu.edu.cn>
2022-11-19 12:17:07 +0800

    by zx

--- bgp_config.py
+++ bgp_config.py
@@ -52,9 +52,34 @@ def del_vlan_list():
     else:
         print('Failed to connect to switch')
 
+def ip_config():   
+    commands = []
+    port_list = []
+    ip_list = []
+    for i in range(50, 65):
+        port_list.append(i)
+    for i in range(2, 17):
+        ip_list.append(i)
+    commands.append('config')
+    for port_num in range(len(port_list)):
+        commands.append("interface Hu0/%s" % port_list[port_num])
+        commands.append("no switchport")
+        commands.append("ip address 172.167.%s.2 255.255.255.0" % ip_list[port_num])
+        commands.append("show this")
+        commands.append("exit")
+
+    commands.append('exit')
+    commands.append('exit')
+
+    telnet_client = TelnetClient()
+    if telnet_client.login_host(host_ip, password):
+        for cmd in commands:
+            telnet_client.execute_some_command(cmd, interval=0.5)
+    else:
+        print('Failed to connect to switch')
 
 if __name__ == "__main__":
-    del_vlan_list()
+    ip_config()
 
 
 
