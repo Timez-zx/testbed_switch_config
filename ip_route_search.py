2a47c67d49b57af3bfb62e5d71996176e09a1c9c
zx <zhangxiao123@sjtu.edu.cn>
2022-11-19 19:59:57 +0800

    by zx

--- ip_route_search.py
+++ ip_route_search.py
@@ -5,15 +5,19 @@ def get_table():
     p = os.popen('ip rule', 'r', 1)
     rule = p.read().split('\n')
     data =  [i for i in rule if i and not i.startswith('lo')]
-    table = []
+    table = {}
     for line in data:
         num = line.split()[-1]
+
+        ip = line.split()[-3]
         if(num not in table):
-            if(num[0] == '4'):
-                table.append(num)
+            if((num[0] == '4') & (ip[0] == '1') ):
+                table[ip] = num
          
     return table
+
+
         
 
 if __name__ == '__main__':
-    get_table()
+    print(get_table())
